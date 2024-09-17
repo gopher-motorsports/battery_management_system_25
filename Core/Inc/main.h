b8497db4e8e84e94f17dc9e236dd71189a1aa0f2
Sabrina Simkhovich <sabrinasimkhovich@gmail.com>
2024-09-16 21:45:57 -0500

    adbms files added

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -57,6 +57,16 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define PORTA_CS_Pin GPIO_PIN_4
+#define PORTA_CS_GPIO_Port GPIOA
+#define ISO_SPI_CLK_Pin GPIO_PIN_5
+#define ISO_SPI_CLK_GPIO_Port GPIOA
+#define ISO_SPI_MISO_Pin GPIO_PIN_6
+#define ISO_SPI_MISO_GPIO_Port GPIOA
+#define ISO_SPI_MOSI_Pin GPIO_PIN_7
+#define ISO_SPI_MOSI_GPIO_Port GPIOA
+#define PORTB_CS_Pin GPIO_PIN_4
+#define PORTB_CS_GPIO_Port GPIOC
 
 /* USER CODE BEGIN Private defines */
 
